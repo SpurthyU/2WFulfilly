1ac1fe4cde3372e723cd592440ba3f4fad1014d0
SpurthyU <155438771+SpurthyU@users.noreply.github.com>
2024-05-31 13:52:52 +0530

    Add files via upload

---
+++ fulfilly.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+
+# Function to read Excel file
+def read_excel(file):
+    df = pd.read_excel(file)
+    df['Date And Time'] = pd.to_datetime(df['Date And Time'])
+    df.sort_values(by='Date And Time', ascending=False, inplace=True)
+    df = df.drop_duplicates(subset='Vehicle Number', keep='first')
+    selected_columns = ['Vehicle Number', 'Date And Time', 'Location', 'Voltage (V)', 'Current (A)', 'State Of Charge (%)']
+    df = df[selected_columns]
+    return df
+
+def main():
+    st.title('FULFILLY')
+
+    uploaded_file = st.file_uploader("Upload your Excel file", type=["xlsx"])
+
+    if uploaded_file is not None:
+        df = read_excel(uploaded_file)
+        st.write(df)
+
+if __name__ == "__main__":
+    main()
